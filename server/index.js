448ad9411672a4b60b04be89baf4214aec50817f
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-08 00:56:04 +0000

    Console result from db query

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ function checkInCheck() {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(`HHEEEEELLLOOO!: ${result.rows}`);
+    console.log(`HHEEEEELLLOOO!: ${result}`);
     client.query("SELECT contact_id FROM contacts WHERE owner_id = (" + result.rows[0].id + ")", (err, result) => {
       if (err) {
         return console.error("error running query", err);
