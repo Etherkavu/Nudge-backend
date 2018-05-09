a33947dd2cd9614ca2a39cc160c5a70ba5d8036a
Brian Holden <bkavuh@gmail.com>
2018-05-09 00:24:43 +0000

    testing add contact

--- server.js
+++ server.js
@@ -156,32 +156,32 @@ var corsOptions = {
 app.get("/ping", cors(corsOptions), (req, res) => {
   console.log("Hey look we made it here");
   activeusers['moo@moo.moo'] = {count: 0};
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.get("/login/:id", cors(corsOptions), (req, res) => {
   activeusers[req.params.id] = {count : 0}
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.get("/logout/:id", cors(corsOptions), (req, res) => {
   delete activeusers[req.params.id];
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.get("/update/:id", cors(corsOptions), (req, res) => {
   addContact(req.params.id, 'bkavuh@gmail.com', 'jeff');
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.get("/register", cors(corsOptions), (req, res) => {
   register(req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.contact_name, req.body.contact_email);
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.get("/update/:id", cors(corsOptions), (req, res) => {
   updateContact(req.params.id, req.body.email, req.body.name);
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
