e6aad880802dbd4ecd6ec83879a99cfc8141ea99
Brian Holden <bkavuh@gmail.com>
2018-05-10 00:23:10 +0000

    pull contacts works

--- server.js
+++ server.js
@@ -259,6 +259,7 @@ app.post("/contacts", cors(corsOptions), (req, res) => {
   // register(req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.contact_name, req.body.contact_email);
   console.log("first param:", req.body.firstParam);
   console.log("req body:", req.body);
+  console.log("req:", req);
   res.sendStatus(200);
 });
 
