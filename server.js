9a23ad8e061462028bfe60ae6cec751c9c647739
Brian Holden <bkavuh@gmail.com>
2018-05-10 20:02:08 +0000

    testing google remote server auth

--- server.js
+++ server.js
@@ -232,7 +232,7 @@ app.post("/contacts", (req, res, next) => {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-      console.log(JSON.parse(data).explanation);
+      console.log(data);
     });
 
   }).on("error", (err) => {
