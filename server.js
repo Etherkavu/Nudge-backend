2b32a49807f283bef2803af92ca4a0199243f256
Brian Holden <bkavuh@gmail.com>
2018-05-14 22:34:31 +0000

    testing emailer

--- server.js
+++ server.js
@@ -125,8 +125,8 @@ var contact;
 function sendEmail(email, user){
   app.mailer.send('email', {
     to: email, // REQUIRED. This can be a comma delimited string just like a normal email to field.
-    subject: 'Inactive notification', // REQUIRED.
-    text: '\nUser: '+ user + 'has gone inactive, please check in on them. \n\nNotification sent automatically from nudge server, do not reply.'
+    subject: 'User inactivity notification', // REQUIRED.
+    text: '\nUser: '+ user + ' has gone inactive, please check in on them. \n\nNotification sent automatically from nudge server, do not reply.'
    }, function (err) {
      if (err) {
        // handle error
